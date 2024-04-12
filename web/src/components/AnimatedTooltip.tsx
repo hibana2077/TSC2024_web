67783cdf9fac72d5c62abc59a3d46b2050c30db7
hibana2077 <hibana2077@gmail.com>
2024-04-12 22:25:14 +0800

    Add new hosts to AnimatedTooltip component and update contact information in Footer component

--- web/src/components/AnimatedTooltip.tsx
+++ web/src/components/AnimatedTooltip.tsx
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmail.com
  * @Date: 2024-04-12 21:01:33
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2024-04-12 21:52:09
+ * @LastEditTime: 2024-04-12 22:13:21
  * @FilePath: \TSC2024_web\web\src\components\AnimatedTooltip.tsx
  * @Description: 
  */
@@ -58,6 +58,34 @@ const people = [
     designation: "中興資安社 社長",
     image:
       "https://cdn.discordapp.com/avatars/493487434823237684/8535b0600c7a88d43090eba956a09010.webp?size=128",
+  },
+  {
+    id: 8,
+    name: "Gunjyo",
+    designation: "（清大）清資社 社長",
+    image:
+      "https://cdn.discordapp.com/avatars/721026352878452737/ab4b7db90ed8c24f2964518590427f0a.webp?size=128",
+  },
+  {
+    id: 9,
+    name: "D",
+    designation: "嘉大駭客社 社長",
+    image:
+      "https://cdn.discordapp.com/avatars/996733773100109835/da41757193056551cf3c627a97506376.webp?size=128",
+  },
+  {
+    id: 10,
+    name: "AN",
+    designation: "台科資安社 社長",
+    image:
+      "https://cdn.discordapp.com/avatars/568399454298963968/ce42641853c50ade30456c1f054f75dd.webp?size=128",
+  },
+  {
+    id: 11,
+    name: "Terry1234",
+    designation: "中正資安社 社長",
+    image:
+      "https://cdn.discordapp.com/avatars/376700718641577985/5bc9cf321eaef996826809f9a9b70791.webp?size=128",
   }
 ];
 
